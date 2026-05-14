6bc25056c0c0d2c550b66b1ae559eab8f6523ce0
Raoni Barbosa <raoni@totalicontabilidade.com.br>
2026-05-14 15:50:18 -0300

    etapa 6: DRE + DFC (reports module, páginas, schema dfcType + subtipo categoria)

---
+++ backend/prisma/migrations/20260509103638_add_categoria_subtipo/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "categories" ADD COLUMN     "subtipo" VARCHAR(30) NOT NULL DEFAULT 'operacional';
---
+++ backend/prisma/migrations/20260514184909_add_dfc_type_to_category/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "categories" ADD COLUMN     "dfc_type" VARCHAR(20) DEFAULT 'OPERACIONAL';
